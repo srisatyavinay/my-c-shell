af1e39ffb82876adfa118a9aa5df88ebd254a611
srisatyavinay <81536213+srisatyavinay@users.noreply.github.com>
2021-10-09 20:52:17 +0530

    Add bg execution

--- bg_execution.c
+++ bg_execution.c
@@ -2,5 +2,36 @@
 
 void bg_execution(char **arg, int num)
 {
-    
+    if(num == 2)
+    {
+        int njob = atoi(arg[1]);
+        int jpid;
+
+        int found = 0;
+
+        struct back *jobptr = present;
+
+        while(jobptr != NULL)
+        {
+            if(jobptr->jobnum == njob)
+            {
+                found = 1;
+                jpid = jobptr->backpid;
+                break;
+            }
+        }
+        if(found == 1)
+        {
+            // kill(jpid, SIGTTIN);
+            kill(jpid, SIGCONT);
+        }
+        else
+        {
+            fprintf(stderr, "Enter valid job number");
+        } 
+    }
+    else
+    {
+        fprintf(stderr, "jobs: Incorrect syntax");
+    }
 }
